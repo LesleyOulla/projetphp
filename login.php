7055079391b02778359e05b70d9963d434772200
lesley <lesleyoulla@icloud.com>
2023-03-21 13:49:42 +0100

    (feature/front-register) j'ai modifié html afin de faire le register/login

--- login.php
+++ login.php
@@ -1,25 +1,4 @@
-<?php 
 
-// il y a une obligation
-require('formulaire.php');
-//appel de la classe dans la fonction
-$form = new form();
-function Input($size, $name, $label, $type, $placeholder) {
-    $un = '<div class="col-md-'.$size.'">
-                <div class="mb-3">';
-    $deux = '';
-    $trois =  '<input type="'.$type.'" name="'.$name.'" class="form-control" id="'.$name.'" placeholder="'.$placeholder.'">
-                </div>
-            </div>';
-
-    if ($type != 'submit') {
-
-        $deux = '<label for="'.$name.'" class="form-label">'.$label.'</label>';
-    }
-
-    return $un.$deux.$trois;
-}
-    ?>
 <!doctype html>
 <html lang="fr">
   <head>
@@ -37,10 +16,8 @@ function Input($size, $name, $label, $type, $placeholder) {
         <form class="row" action="result.php" method="get">
                  <?php 
                  // appelez la fonction avec "$form->"
-                 echo $form->Input("4", "nom", "Votre nom", "text", "Entrer un nom");
-                 echo $form->Input("4", "prenom", "Votre prenom", "text", "Entrer un prenom");
+
                  echo $form->Input("4", "email", "Votre email", "email", "Entrer un email");
-                 echo $form->Input("4", "telephone", "Votre téléphone", "tel", "Entrer un téléphone");
                  echo $form->Input("4", "password", "Votre mot de passe", "password", "Entrer un mot de passe");
                  echo $form->Input("4", "password2", "Votre confirmation de mot de passe", "password", "Entrer un mot de passe");
                  echo $form->Input("4", "Envoyer", "Envoyer", "submit", "");
@@ -51,16 +28,4 @@ function Input($size, $name, $label, $type, $placeholder) {
     </footer>
   </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
 
